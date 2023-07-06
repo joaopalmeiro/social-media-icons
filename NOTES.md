ca67986597819975a19112cc79d0e8249d141be7
João Palmeiro <joaommpalmeiro@gmail.com>
2023-07-06 11:44:06 +0100

    Update SVGO config

--- NOTES.md
+++ NOTES.md
@@ -6,3 +6,7 @@
 - https://github.com/svg/svgo/issues/1211
 - https://github.com/lucide-icons/lucide/blob/main/icons/instagram.svg?short_path=e435f4b
 - https://github.com/svg/svgo/releases/tag/v3.0.0
+- https://github.com/best-shot/svgo-config
+- https://publint.dev/ + https://publint.dev/rules
+- https://www.stefanjudis.com/today-i-learned/vs-code-supports-jsdoc-powered-type-checking/
+- https://github.com/svg/svgo#built-in-plugins
--- svgo.config.js
+++ svgo.config.js
@@ -1,5 +1,6 @@
-// https://github.com/svg/svgo#built-in-plugins
-
+/**
+ * @type {import('svgo').Config}
+ */
 module.exports = {
   multipass: true,
 
@@ -9,34 +10,34 @@ module.exports = {
       params: {
         overrides: {
           removeViewBox: false,
+          // Source: https://github.com/svg/svgo/blob/master/plugins/sortAttrs.js
+          sortAttrs: {
+            xmlnsOrder: "alphabetical",
+          },
+          // Source: https://github.com/svg/svgo/blob/main/plugins/convertColors.js
+          convertColors: {
+            currentColor: true,
+          },
         },
       },
     },
 
     // Source: https://github.com/svg/svgo/blob/main/plugins/removeRasterImages.js
     "removeRasterImages",
+    // Source: https://github.com/svg/svgo/blob/main/plugins/removeScriptElement.js
+    "removeScriptElement",
+    // Source: https://github.com/svg/svgo/blob/main/plugins/removeStyleElement.js
+    "removeStyleElement",
+    // Source: https://github.com/svg/svgo/blob/main/plugins/reusePaths.js
+    "reusePaths",
+    // Source: https://github.com/svg/svgo/blob/main/plugins/removeOffCanvasPaths.js
+    "removeOffCanvasPaths",
 
     // https://github.com/svg/svgo/blob/main/plugins/removeAttrs.js
     {
       name: "removeAttrs",
       params: {
         attrs: "svg:fill:none",
-      },
-    },
-
-    // Source: https://github.com/svg/svgo/blob/master/plugins/sortAttrs.js
-    {
-      name: "sortAttrs",
-      params: {
-        xmlnsOrder: "alphabetical",
-      },
-    },
-
-    // Source: https://github.com/svg/svgo/blob/main/plugins/convertColors.js
-    {
-      name: "convertColors",
-      params: {
-        currentColor: true,
       },
     },
   ],

f0bbed595cd99bc8b43e39b9f2fa5f3e969f55b9
João Palmeiro <joaommpalmeiro@gmail.com>
2023-10-22 19:35:14 +0100

    Add picosvg script

--- apply_picosvg.py
+++ apply_picosvg.py
@@ -1,8 +1,38 @@
 # https://github.com/googlefonts/picosvg/blob/v0.22.1/setup.py#L25
 # https://github.com/googlefonts/picosvg/blob/v0.22.1/src/picosvg/picosvg.py#L74
 # https://github.com/googlefonts/picosvg/blob/v0.22.1/src/picosvg/picosvg.py#L46
+# https://stackoverflow.com/questions/50110800/python-pathlib-make-directories-if-they-don-t-exist
+# https://github.com/googlefonts/picosvg/blob/v0.22.1/src/picosvg/svg.py#L1332
+
+from pathlib import Path
 
 from picosvg.svg import SVG
 
+INPUT_PATH = Path("figma/icon")
+# OUTPUT_PATH = Path("figma/icon")
+OUTPUT_PATH = Path("picosvg")
+
+# https://bjango.com/articles/svgpassthroughprecision/#figma
+# https://bjango.com/articles/svgexports/
+# https://bjango.com/articles/svgpassthroughprecision/#other-tests
+# Figma:
+# SVG_EXPORT_PRECISION = 5
+# Maximum value found:
+SVG_EXPORT_PRECISION = 10
+
 if __name__ == "__main__":
-    print(SVG)
+    OUTPUT_PATH.mkdir(parents=True, exist_ok=True)
+
+    for icon_path in INPUT_PATH.glob("*.svg"):
+        print(icon_path)
+        icon = SVG.parse(icon_path)
+
+        processed_icon = icon.topicosvg(
+            ndigits=SVG_EXPORT_PRECISION, allow_text=False, drop_unsupported=False
+        )
+        # print(processed_icon)
+
+        output_icon = processed_icon.tostring(pretty_print=True)
+
+        with open(OUTPUT_PATH / icon_path.name, "w") as f:
+            f.write(output_icon)

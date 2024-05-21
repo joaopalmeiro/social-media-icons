dc36b0454a1d104507e21442074b1e5dd096ef13
João Palmeiro <joaommpalmeiro@gmail.com>
2024-05-21 09:04:35 +0100

    Update svgo.config.mjs

--- svgo.config.mjs
+++ svgo.config.mjs
@@ -2,6 +2,8 @@
  * @type {import('svgo').Config}
  */
 export default {
+  // https://github.com/simple-icons/simple-icons/blob/11.15.0/svgo.config.mjs
+
   multipass: true,
   eol: "lf",
 
@@ -32,5 +34,73 @@ export default {
         spaces: true,
       },
     },
+    // https://svgo.dev/docs/plugins/mergeStyles/
+    "mergeStyles",
+    // https://svgo.dev/docs/plugins/inlineStyles/
+    {
+      name: "inlineStyles",
+      params: {
+        onlyMatchedOnce: true,
+        removeMatchedSelectors: true,
+      },
+    },
+    // https://svgo.dev/docs/plugins/minifyStyles/
+    {
+      name: "minifyStyles",
+      params: {
+        usage: true,
+      },
+    },
+    // https://svgo.dev/docs/plugins/cleanupIds/
+    {
+      name: "cleanupIds",
+      params: {
+        remove: true,
+        minify: true,
+        preserve: [],
+        preservePrefixes: [],
+        force: false,
+      },
+    },
+    // https://svgo.dev/docs/plugins/removeUselessDefs/
+    "removeUselessDefs",
+    // https://svgo.dev/docs/plugins/cleanupNumericValues/
+    {
+      name: "cleanupNumericValues",
+      params: {
+        floatPrecision: 3,
+        leadingZero: true,
+        defaultPx: true,
+        convertToPx: true,
+      },
+    },
+    // https://svgo.dev/docs/plugins/convertColors/
+    {
+      name: "convertColors",
+      params: {
+        currentColor: false,
+        names2hex: true,
+        rgb2hex: true,
+        convertCase: "lower",
+        shorthex: true,
+        shortname: true,
+      },
+    },
+    // https://svgo.dev/docs/plugins/removeUnknownsAndDefaults/
+    {
+      name: "removeUnknownsAndDefaults",
+      params: {
+        unknownContent: true,
+        unknownAttrs: true,
+        defaultAttrs: true,
+        defaultMarkupDeclarations: true,
+        uselessOverrides: true,
+        keepDataAttrs: false,
+        keepAriaAttrs: true,
+        keepRoleAttr: true,
+      },
+    },
+    // https://svgo.dev/docs/plugins/removeNonInheritableGroupAttrs/
+    "removeNonInheritableGroupAttrs",
   ],
 };
